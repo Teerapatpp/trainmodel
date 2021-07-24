cad0ca61d08bbe8f360eeaceb9b64e0842181100
aeff60 <ssbunklang@gmail.com>
2021-07-24 15:48:09 +0700

    delete Optional value

--- trainmodel.py
+++ trainmodel.py
@@ -11,26 +11,20 @@ iris = load_iris()
 iris['data']
 iris['target']
 
-
 X, y = iris['data'], iris['target']
-dataset = np.hstack((X, y.reshape(-1,1)))
+dataset = np.hstack((X, y.reshape(-1, 1)))
 
 np.random.shuffle(dataset)
-X_train, X_test, y_train, y_test = train_test_split(dataset[:,:4],
-                                                   dataset[:,4],
-                                                   test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(dataset[:, :4],
+                                                    dataset[:, 4],
+                                                    test_size=0.2)
 
 model = LogisticRegression()
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
 
-#print("predied is : "y_pred)
+acc = accuracy_score(y_test, y_pred)
+print("Accuracy sore is :", acc)
 
-#acc =accuracy_score(y_test, y_pred)
-#print("Accuracy sore is :",acc )
-
-#result = model.predict([[5.1, 3.5, 1.4, 0.2]])
-#print(result)
-
-joblib.dump(model,'iris.model')
+joblib.dump(model, 'iris.model')
